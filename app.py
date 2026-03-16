b3aa63f5557af16180d9ae2341e01e5ec391990a
johanmare78-cmd <johanmare78@gmail.com>
2026-03-16 13:12:52 +0200

    app.py

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 import streamlit as st
 import pandas as pd
 from streamlit_autorefresh import st_autorefresh
-
-from config import REFRESH_INTERVAL
-from utils.calculations import calculate_stats
 from dashboard.leaderboard import show_leaderboard
 
+REFRESH_INTERVAL = 15000
 
 st.set_page_config(page_title="Agent Stats", layout="wide")
 
 # Auto refresh
 st_autorefresh(interval=REFRESH_INTERVAL)
 
-# Clear cache so Excel reloads
-st.cache_data.clear()
+def calculate_stats(df):
+
+    df["Weekly Need"] = df["Weekly Target"] - df["Weekly Done"]
+    df["Monitor Need"] = df["Monitor Target"] - df["Monitor On"]
+    df["Commission Need"] = df["Commission Target"] - df["Commission On"]
+
+    return df
 
 try:
 
-    # Load Excel directly
     df = pd.read_excel("Agent_Dashboard.xlsx")
 
-    # Run calculations
     df = calculate_stats(df)
 
-    # Show dashboard
     show_leaderboard(df)
 
 except Exception as e:
